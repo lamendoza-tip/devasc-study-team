5fe51757cedfd4b06786057fb464eada7eb65d2d
pajadura-tip <qkcppajadura@tip.edu.ph>
2021-11-06 05:21:58 +0000

    modified IPaddapp.py added option

--- IPaddapp.py
+++ IPaddapp.py
@@ -1,17 +1,33 @@
+#PAJADURA, MENDOZA, TADIWAN
+
 import urllib.parse
 import requests
 
-main_api = "http://ip-api.com/json/"
-url = main_api
-print("=============================================")
-print("URL: " +url)
-json_data = requests.get(url).json()
-print("=============================================")
-print("Public IP: " + (json_data["query"]))
-print("Country: " + str(json_data["country"]))
-print("Country Code: " + str(json_data["countryCode"]))
-print("Region: " + str(json_data["regionName"]))
-print("ISP: " + str(json_data["isp"]))
-print("ASN & Provider: " + str(json_data["as"]))
-print("=============================================")
+while True:
+    print("Press Y if you want to check your own IP")
+    print("Press N to quit")
+    initial = input("Enter here: ")
 
+
+    # =========================== Option 1 =========================== 
+    if initial == "Y" or initial == "y":
+        main_api = "http://ip-api.com/json/"
+        url = main_api
+        print("=============================================")
+        print("URL: " +url)
+
+        # Get JSON Data
+        json_data = requests.get(url).json()
+        print("=============================================")
+        print("Public IP: " + (json_data["query"]))
+        print("Country: " + str(json_data["country"]))
+        print("Country Code: " + str(json_data["countryCode"]))
+        print("Region: " + str(json_data["regionName"]))
+        print("ISP: " + str(json_data["isp"]))
+        print("ASN & Provider: " + str(json_data["as"]))
+        print("=============================================")
+
+
+    # =========================== Option 2 =========================== 
+    if initial == "N" or initial == "n":
+        break
